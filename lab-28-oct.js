27762f5fa3e84984352479d64406578b847039ac
Steven Hall <steven@hallys.net>
2021-10-28 10:00:42 +1000

    File modified for Lab on 28 Oct

---
+++ lab-28-oct.js
@@ -0,0 +1,22 @@
+/*
+ Please solve the following problems:
+*/
+///////////////////////////////////////////////////////////////
+// Given an array of integers, find the sum of its elements.
+///////////////////////////////////////////////////////////////
+let data=[1,2,3,4,10,11];   // should result in 31
+function simpleArraySum(){
+    // TODO
+}
+let result=simpleArraySum(data);
+console.log(result);
+
+///////////////////////////////////////////////////////////////
+// Given an array of names return an array sorted alphabetically
+///////////////////////////////////////////////////////////////
+let names=["Steven","Himanshu", "Elo","Keith","Saikran","Sahej"];
+
+function sortAlphabetically(arr){
+    // TODO
+}
+console.log(sortAlphabetically(names));
---
+++ readme.md
@@ -0,0 +1,22 @@
+# This repository is for THS lab work
+
+To work on the assigned lab problem
+
+delete any previous copies of the ths_chort_G_labs directory
+Clone this repository:
+```
+git clone https://github.com/pc9geek/ths_chort_G_labs.git
+```
+Create a branch to work on
+```
+git branch steve
+git checkout steve
+```
+work on the files and when you are completed
+```
+git add .
+git commit -m"my changes"
+git push
+```
+
+the main repository will then have a copy of each student's branch to review
